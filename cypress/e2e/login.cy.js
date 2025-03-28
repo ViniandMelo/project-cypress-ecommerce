5b1062d062a96ce52a0cf31b8ccff2b2c0c21077
ViniandMelo <viniciusdemelo8@gmail.com>
2025-03-28 19:11:28 -0300

    CI Workflow created

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,58 +1,70 @@
-import userData from '../fixtures/users/userData.json'; // Importa os dados do usuário para uso nos testes
-import { selectorsList } from '../selectors/loginSelectors'; // Importa os seletores da página de login
+import userData from "../fixtures/users/userData.json"; // Importa os dados do usuário para uso nos testes
+import { selectorsList } from "../selectors/loginSelectors"; // Importa os seletores da página de login
 
-const Chance = require('chance') // Importa a biblioteca Chance para gerar dados aleatórios
-const chance = new Chance()
+const Chance = require("chance"); // Importa a biblioteca Chance para gerar dados aleatórios
+const chance = new Chance();
 
-describe('pagina de login', () => {
+describe("pagina de login", () => {
+  it("Registrar Usuario", () => {
+    cy.visitAndClickLogin(); // Visita a página inicial e clica no botão de login
+    cy.get("h2").contains("New User Signup!"); // Verifica se o texto "New User Signup!" está visível
+    cy.location("pathname").should("eq", "/login"); // Verifica se a URL contém '/login'
+    cy.get(selectorsList.usernamField).type(chance.name()); // Preenche o campo de nome de usuário
+    cy.get(selectorsList.emailField).type(chance.email()); // Preenche o campo de email
+    cy.get(selectorsList.regiterButton).click(); // Clica no botão de registro
 
-  it('Registrar Usuario', () => {
-    cy.visitAndClickLogin() // Visita a página inicial e clica no botão de login
-    cy.get('h2').contains('New User Signup!') // Verifica se o texto "New User Signup!" está visível
-    cy.location('pathname').should('eq', '/login') // Verifica se a URL contém '/login'
-    cy.get(selectorsList.usernamField).type(chance.name()) // Preenche o campo de nome de usuário
-    cy.get(selectorsList.emailField).type(chance.email()) // Preenche o campo de email
-    cy.get(selectorsList.regiterButton).click() // Clica no botão de registro
+    cy.get(selectorsList.bSelector).contains("Enter Account Information"); // Verifica se o texto "Enter Account Information" está visível
+    cy.get(selectorsList.imputGender).click(); // Seleciona o gênero (masculino/feminino)
+    cy.get(selectorsList.passwordField).type(userData.userSucess.userpassword); // Preenche o campo de senha
+    cy.get(selectorsList.daysSelector).select(userData.userSucess.userday); // Seleciona o dia de nascimento
+    cy.get(selectorsList.monthsSelector).select(userData.userSucess.usermonth); // Seleciona o mês de nascimento
+    cy.get(selectorsList.yearsSelector).select(userData.userSucess.useryear); // Seleciona o ano de nascimento
 
-    cy.get(selectorsList.bSelector).contains('Enter Account Information') // Verifica se o texto "Enter Account Information" está visível
-    cy.get(selectorsList.imputGender).click() // Seleciona o gênero (masculino/feminino)
-    cy.get(selectorsList.passwordField).type(userData.userSucess.userpassword) // Preenche o campo de senha
-    cy.get(selectorsList.daysSelector).select(userData.userSucess.userday) // Seleciona o dia de nascimento
-    cy.get(selectorsList.monthsSelector).select(userData.userSucess.usermonth) // Seleciona o mês de nascimento
-    cy.get(selectorsList.yearsSelector).select(userData.userSucess.useryear) // Seleciona o ano de nascimento
+    cy.get(selectorsList.newsletterContains).contains(
+      "Sign up for our newsletter!",
+    ); // Verifica o texto do checkbox de newsletter
+    cy.get(selectorsList.newsletterCheckbox).click(); // Marca o checkbox de newsletter
+    cy.get(selectorsList.addressCheckbox).click(); // Marca o checkbox de ofertas especiais
 
-    cy.get(selectorsList.newsletterContains).contains('Sign up for our newsletter!') // Verifica o texto do checkbox de newsletter
-    cy.get(selectorsList.newsletterCheckbox).click() // Marca o checkbox de newsletter
-    cy.get(selectorsList.addressCheckbox).click() // Marca o checkbox de ofertas especiais
+    cy.get(selectorsList.bSelector).contains("Address Information"); // Verifica se o texto "Address Information" está visível
+    cy.get(selectorsList.firstnameField).type(chance.first()); // Preenche o campo de primeiro nome
+    cy.get(selectorsList.lastnameField).type(chance.last()); // Preenche o campo de sobrenome
+    cy.get(selectorsList.companyField).type(chance.company()); // Preenche o campo de empresa
+    cy.get(selectorsList.adressOneField).type(userData.userSucess.userStreet); // Preenche o campo de endereço 1
+    cy.get(selectorsList.adressTwoField).type(
+      userData.userSucess.userStreetTwo,
+    ); // Preenche o campo de endereço 2
+    cy.get(selectorsList.countrySelector).select(
+      userData.userSucess.userCountry,
+    ); // Seleciona o país
+    cy.get(selectorsList.stateSelector).type(userData.userSucess.userState); // Preenche o campo de estado
+    cy.get(selectorsList.citySelector).type(userData.userSucess.userCity); // Preenche o campo de cidade
+    cy.get(selectorsList.postalCodeField).type(
+      userData.userSucess.userPostalCode,
+    ); // Preenche o campo de CEP
+    cy.get(selectorsList.mobileNumberField).type(
+      userData.userSucess.userNumber,
+    ); // Preenche o campo de número de telefone
+    cy.get(selectorsList.submitButton).click(); // Clica no botão de submit
 
-    cy.get(selectorsList.bSelector).contains('Address Information') // Verifica se o texto "Address Information" está visível
-    cy.get(selectorsList.firstnameField).type(chance.first()) // Preenche o campo de primeiro nome
-    cy.get(selectorsList.lastnameField).type(chance.last()) // Preenche o campo de sobrenome
-    cy.get(selectorsList.companyField).type(chance.company()) // Preenche o campo de empresa
-    cy.get(selectorsList.adressOneField).type(userData.userSucess.userStreet) // Preenche o campo de endereço 1
-    cy.get(selectorsList.adressTwoField).type(userData.userSucess.userStreetTwo) // Preenche o campo de endereço 2
-    cy.get(selectorsList.countrySelector).select(userData.userSucess.userCountry) // Seleciona o país
-    cy.get(selectorsList.stateSelector).type(userData.userSucess.userState) // Preenche o campo de estado
-    cy.get(selectorsList.citySelector).type(userData.userSucess.userCity) // Preenche o campo de cidade
-    cy.get(selectorsList.postalCodeField).type(userData.userSucess.userPostalCode) // Preenche o campo de CEP
-    cy.get(selectorsList.mobileNumberField).type(userData.userSucess.userNumber) // Preenche o campo de número de telefone
-    cy.get(selectorsList.submitButton).click() // Clica no botão de submit
-
-    cy.get(selectorsList.bSelector).contains(userData.userError.createMessage) // Verifica a mensagem de sucesso após o registro
-    cy.get(selectorsList.continueButtonOne).click() // Clica no botão "Continue" após o registro
-    cy.get(selectorsList.deleteButton).click() // Clica no botão para deletar a conta
-    cy.get(selectorsList.continueButtonTwo).click() // Clica no botão "Continue" após deletar a conta
+    cy.get(selectorsList.bSelector).contains(userData.userError.createMessage); // Verifica a mensagem de sucesso após o registro
+    cy.get(selectorsList.continueButtonOne).click(); // Clica no botão "Continue" após o registro
+    cy.get(selectorsList.deleteButton).click(); // Clica no botão para deletar a conta
+    cy.get(selectorsList.continueButtonTwo).click(); // Clica no botão "Continue" após deletar a conta
   });
 
-  it('Login Usuario', () => {
-    cy.visitAndClickLogin() // Visita a página inicial e clica no botão de login
-    cy.login(userData.userSucess.useremalogin, userData.userSucess.userpassword) // Faz login com email e senha válidos
-    cy.get(selectorsList.deleteButton).should('be.visible') // Verifica se o botão de deletar conta está visível (indica que o login foi bem-sucedido)
+  it("Login Usuario", () => {
+    cy.visitAndClickLogin(); // Visita a página inicial e clica no botão de login
+    cy.login(
+      userData.userSucess.useremalogin,
+      userData.userSucess.userpassword,
+    ); // Faz login com email e senha válidos
+    cy.get(selectorsList.deleteButton).should("be.visible"); // Verifica se o botão de deletar conta está visível (indica que o login foi bem-sucedido)
   });
 
-  it('Login Usuario Incorreto', () => {
-    cy.visit('') // Visita a página inicial sem clicar no botão de login
-    cy.login(chance.email(), userData.userError.userpasswordincorrect) // Tenta fazer login com email e senha incorretos
-    cy.verifyMessage(selectorsList.alertText, userData.userError.errorMessage) // Verifica se a mensagem de erro é exibida
+  it("Login Usuario Incorreto", () => {
+    cy.visit(""); // Visita a página inicial sem clicar no botão de login
+    cy.login(chance.email(), userData.userError.userpasswordincorrect); // Tenta fazer login com email e senha incorretos
+    cy.verifyMessage(selectorsList.alertText, userData.userError.errorMessage); // Verifica se a mensagem de erro é exibida
   });
 });
