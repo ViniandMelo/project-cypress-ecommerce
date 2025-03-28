e36c40c2b5e9aab2cf6dbd3d342dabcec1ee811e
ViniandMelo <viniciusdemelo8@gmail.com>
2025-03-28 20:39:19 -0300

    Atualizando arquivos de evidencia dos testes

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,11 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "https://automationexercise.com/"
+    baseUrl: "https://automationexercise.com/",
+    video: true, // Habilita a gravação de vídeos
+    screenshotOnRunFailure: true, // Faz screenshot se o teste falhar
+    screenshotsFolder: "cypress/screenshots", // Pasta para salvar screenshots
+    videosFolder: "cypress/videos", // Pasta para salvar vídeos
+    resultsFolder: "cypress/results", // Pasta para salvar resultados (caso queira salvar arquivos de resultados)
   },
 });
